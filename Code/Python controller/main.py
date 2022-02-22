edb9555e3f0234e19fb69e1f4e9d2f0bfea22c65
NicAU9 <augn1301@usherbrooke.ca>
2022-02-22 16:36:34 -0500

    Merge comm + UI

--- Code/Python controller/main.py
+++ Code/Python controller/main.py
@@ -5,6 +5,8 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
 from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
 from PyQt5.QtWidgets import *
+import communication
+from order import Order
 
 # GUI FILE
 from ui_main import Ui_MainWindow
@@ -14,6 +16,17 @@ class MainWindow(QMainWindow):
         QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.setWindowModality(QtCore.Qt.ApplicationModal)
+
+        ## For comm
+        self.comm = communication.Communication()
+        self.listcmd = []
+        self.listdist = []
+
+        ## For UI
+        self.listangle = []
+        self.counter = 0
+
         ## PAGES
         ########################################################################
 
@@ -26,19 +39,71 @@ class MainWindow(QMainWindow):
         # PAGE 3
         self.ui.btn_page_3.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_3))
 
-        self.ui.pb_addcmd.clicked.connect(self.addition)  # assigns the callback fcn of button
+        # Add command button
+        self.ui.pb_addcmd.clicked.connect(self.addcmd)
+
+        # clear command button
+        self.ui.pb_clearcmd.clicked.connect(self.clearcmd)
 
         ## SHOW ==> MAIN WINDOW
         ########################################################################
         self.show()
         ## ==> END ##
 
-    def addition(self):
-        num1 = self.ui.distance_input.text()
-        num2 = self.ui.angle_input_auto.text()
-        res = int(num1) + int(num2)
+    def addcmd(self):
+        dist = self.ui.distance_input.text()
+        angle = self.ui.angle_input_auto.text()
 
-        self.ui.label_state_auto.setText(str(res))
+        if self.counter < 6:
+            self.listcmd.append("Cmd" + str(self.counter + 1) + "-> Dist: " + dist + " Angle: " + angle)
+            self.listdist.append(dist)
+            self.listangle.append(angle)
+            self.counter += 1
+
+        if self.counter == 1:
+            self.ui.label_cmd1.setText(self.listcmd[self.counter-1])
+        elif self.counter == 2:
+            self.ui.label_cmd2.setText(self.listcmd[self.counter-1])
+        elif self.counter == 3:
+            self.ui.label_cmd3.setText(self.listcmd[self.counter-1])
+        elif self.counter == 4:
+            self.ui.label_cmd4.setText(self.listcmd[self.counter-1])
+        elif self.counter == 5:
+            self.ui.label_cmd5.setText(self.listcmd[self.counter-1])
+        elif self.counter == 6:
+            self.ui.label_cmd6.setText(self.listcmd[self.counter-1])
+
+    def clearcmd(self):
+
+        if self.counter > 0:
+            self.listcmd.pop()
+            self.listdist.pop()
+            self.listangle.pop()
+
+        if self.counter == 1:
+            self.ui.label_cmd1.setText(" ")
+        elif self.counter == 2:
+            self.ui.label_cmd2.setText(" ")
+        elif self.counter == 3:
+            self.ui.label_cmd3.setText(" ")
+        elif self.counter == 4:
+            self.ui.label_cmd4.setText(" ")
+        elif self.counter == 5:
+            self.ui.label_cmd5.setText(" ")
+        elif self.counter == 6:
+            self.ui.label_cmd6.setText(" ")
+
+        if self.counter > 0:
+            self.counter -= 1
+
+    #def homeorder(self):
+        #print(self.comm.send_order_param(Order.SERVO, 40, 1))  # une séquence à chaque click + wait for finish
+        #print("end")
+        #print(self.comm.send_order(Order.SEMI_AUTO, 1))
+        #print("end")
+        #print(self.comm.send_order_param(Order.SERVO, 40, 1))
+        #print("end")
+
 
 
 if __name__ == "__main__":
